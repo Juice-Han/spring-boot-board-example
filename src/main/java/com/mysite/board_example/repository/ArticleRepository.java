f58bfc7b20a5b84d628956f0c44efa504a9d59c8
juhanPark <iiiamst16@gmail.com>
2024-10-14 23:28:48 +0900

    repository 어노테이션 추가

--- src/main/java/com/mysite/board_example/repository/ArticleRepository.java
+++ src/main/java/com/mysite/board_example/repository/ArticleRepository.java
@@ -2,6 +2,8 @@ package com.mysite.board_example.repository;
 
 import com.mysite.board_example.entity.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ArticleRepository extends JpaRepository<Article, Integer> {
 }
